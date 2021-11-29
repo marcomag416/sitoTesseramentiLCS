407ed1734aa8eb45ef4c11cbc247f06e21b197b6
Marco Magnanini <marcomag416@gmail.com>
2021-11-29 16:17:03 +0100

    Creata hook funtion useToken

--- tesseramenti/src/functions/useToken.js
+++ tesseramenti/src/functions/useToken.js
@@ -1,21 +1,39 @@
 import { useState } from 'react';
 
 export default function useToken() {
-    const getToken = () => {
-        const tokenString = sessionStorage.getItem('token');
-        const userToken = JSON.parse(tokenString);
-        return userToken?.token
-    };
+    const [token, setToken] = useState();
 
-    const [token, setToken] = useState(getToken());
+    const deleteToken = () => {
+        sessionStorage.removeItem("session");
+        setToken(null);
+        /* fetch delete */
+    }
 
-    const saveToken = userToken => {
-        sessionStorage.setItem('token', JSON.stringify(userToken));
-        setToken(userToken.token);
+    const getToken = useEffect(() => {
+        const tokenString = sessionStorage.getItem('session');
+        const session = JSON.parse(tokenString);
+        if(session == null){
+            setToken(null);
+        }
+        else if(session.time + 14400000 > Date.parse(new Date)){
+            setToken(session.token);
+        }
+        else{
+            deleteToken();
+        }
+    }, )
+
+    const saveToken = (userToken) => {
+        var session = {};
+        session[token] = userToken;
+        session[time] = Date.parse(new Date);
+        sessionStorage.setItem('session', JSON.stringify(session));
+        setToken(userToken);
     };
 
     return {
+        token,
         setToken: saveToken,
-        token
+        deleteToken: deleteToken
     }
 }
