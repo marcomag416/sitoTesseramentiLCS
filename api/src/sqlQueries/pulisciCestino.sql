c4c77e228aff74dca3fd245abceaf9b55fcd2b17
Marco Magnanini <marcomag416@gmail.com>
2021-12-02 21:01:07 +0100

    Creata query sql per pulire il gestino dei giocatori

---
+++ api/src/sqlQueries/pulisciCestino.sql
@@ -0,0 +1,2 @@
+delete from giocatori where giocatori.id in (select gtess.id_giocatore from g_tesserati gtess where gtess.id_stagione = 0);
+delete from g_tesserati where g_tesserati.id_stagione = 0;
