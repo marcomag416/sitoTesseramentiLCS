08e394f5c4f802b35dbb5c7221a613ab6c5e7089
Marco Magnanini <marcomag416@gmail.com>
2021-12-09 17:15:09 +0100

    Create query dirigenti

---
+++ api/src/sqlQueries/insertDirigente.sql
@@ -0,0 +1,3 @@
+INSERT into dirigenti (cf, nome, cognome, id_squadra) values (:cf, :nome, :cognome, :idsquadra);
+insert into d_tesserati (id_stagione, id_giocatore) select :idstagione, 
+(select max(gio.id) from dirigenti dir where dir.cf = :cf and dir.nome = :nome and dir.cognome = :cognome and dir.id_squadra = :idsquadra GROUP by dir.cf);
