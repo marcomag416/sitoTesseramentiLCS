618bc284e32057cb50880b300a3695a76c52165c
Marco Magnanini <marcomag416@gmail.com>
2021-12-26 23:57:28 +0100

    Modifiche a label e alert (barra laterale)

--- tesseramenti/src/components/elem/alert.js
+++ tesseramenti/src/components/elem/alert.js
@@ -1,4 +1,3 @@
-
 function Alert(props){
     var modeStyle;
 
--- tesseramenti/src/components/elem/label.js
+++ tesseramenti/src/components/elem/label.js
@@ -4,13 +4,13 @@ function Label(props){
     const getColorStyle = useMemo(() => {
         switch(props.mode){
             case "r":
-                return "w3-pale-red";
+                return " w3-pale-red w3-border-red";
             case "b":
-                return "w3-pale-blue";
+                return " w3-pale-blue w3-border-blue";
             case "g":
-                return "w3-pale-green";
+                return " w3-pale-green w3-border-green";
             default:
-                return "";
+                return " ";
         }
     }, [props.mode]);
 
@@ -26,7 +26,7 @@ function Label(props){
     }, [props.mode]);
 
     return(
-        <div className = {"w3-panel w3-display-container w3-margin " + getColorStyle}  style={getShow ? { display: 'block' } : { display: 'none' }}>
+        <div className = {"w3-panel w3-leftbar w3-display-container w3-margin " + getColorStyle}  style={getShow ? { display: 'block' } : { display: 'none' }}>
             <span onClick={() => props.onClose()}
                 className="w3-button w3-display-topright">&times;
             </span>
