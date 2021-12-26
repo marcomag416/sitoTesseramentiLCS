d75aff4976373efd08b89b2edb722ad3b07f1419
Marco Magnanini <marcomag416@gmail.com>
2021-12-26 23:57:01 +0100

    Creato elemento alert

---
+++ tesseramenti/src/components/elem/alert.js
@@ -0,0 +1,27 @@
+
+function Alert(props){
+    var modeStyle;
+
+    switch (props.mode){
+        case "alert":
+            modeStyle = "w3-pale-red w3-border-red ";
+            break;
+        case "success":
+            modeStyle = "w3-pale-green w3-border-green ";
+            break;
+        case "warning":
+            modeStyle = "w3-pale-yellow w3-border-yellow ";
+            break;
+        case "info":
+        default:
+            modeStyle = "w3-pale-blue w3-border-blue ";
+    }
+
+    return (
+        <div class= {modeStyle + "w3-panel w3-leftbar"} style = {{width : "95%"}}>
+            <p>{props.msg}</p>
+        </div>
+    );
+}
+
+export default Alert;
