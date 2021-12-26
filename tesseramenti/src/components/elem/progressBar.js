f0c9515789853dc670d7fae15c56b64f404d6883
Marco Magnanini <marcomag416@gmail.com>
2021-12-26 19:28:26 +0100

    implementata progress bar

---
+++ tesseramenti/src/components/elem/progressBar.js
@@ -0,0 +1,19 @@
+import "./progressBar.css";
+
+function ProgressBar(props){
+    const lay3 = props.green + props.red + props.blue;
+    const lay2 = (props.blue + props.green) / (props.green + props.red + props.blue) * 100;
+    const lay1 = props.blue / (props.blue + props.red) * 100;
+
+    return (
+        <div class="holdbar w3-light-grey w3-round">
+            <div class="bar w3-round w3-red" style={{width : lay3 + "%"}}>
+                <div class="bar w3-round w3-green" style={{width : lay2 + "%"}}>
+                    <div class="bar w3-round w3-blue" style={{width : lay1 + "%"}}></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProgressBar;
