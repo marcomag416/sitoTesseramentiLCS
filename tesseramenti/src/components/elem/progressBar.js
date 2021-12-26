5c6b29c4effe335df64869b2fb0d23324800f3a7
Marco Magnanini <marcomag416@gmail.com>
2021-12-27 00:11:54 +0100

    modifiche estetiche alla barra

--- tesseramenti/src/components/elem/progressBar.js
+++ tesseramenti/src/components/elem/progressBar.js
@@ -6,7 +6,7 @@ function ProgressBar(props){
     const [rate, setRate] = useState(0);
     const lay3 = props.green + props.red + props.blue;
     const lay2 = (props.blue + props.green) / (props.green + props.red + props.blue) * 100;
-    const lay1 = props.blue / (props.blue + props.red) * 100;
+    const lay1 = props.blue / (props.blue + props.green) * 100;
 
     useEffect(() => {
         var id = setInterval(() => {
@@ -19,7 +19,7 @@ function ProgressBar(props){
     })
 
     return (
-        <div class="w3-light-grey w3-round" style = {props.style}>
+        <div class="holdbar w3-light-grey w3-round" style = {props.style}>
             <div class="bar w3-round w3-red" style={{width : rate * lay3 + "%"}}>
                 <div class="bar w3-round w3-green" style={{width : rate * lay2 + "%"}}>
                     <div class="bar w3-round w3-blue" style={{width : rate * lay1 + "%"}}></div>
