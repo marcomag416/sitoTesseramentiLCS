56038fe041cbceffdd7de035dfe0ae019e84fc28
Marco Magnanini <marcomag416@gmail.com>
2021-12-26 23:57:41 +0100

    Aggiunta animazione progress bar

--- tesseramenti/src/components/elem/progressBar.js
+++ tesseramenti/src/components/elem/progressBar.js
@@ -1,15 +1,28 @@
 import "./progressBar.css";
+import {useState, useEffect} from 'react';
+
 
 function ProgressBar(props){
+    const [rate, setRate] = useState(0);
     const lay3 = props.green + props.red + props.blue;
     const lay2 = (props.blue + props.green) / (props.green + props.red + props.blue) * 100;
     const lay1 = props.blue / (props.blue + props.red) * 100;
 
+    useEffect(() => {
+        var id = setInterval(() => {
+            if(rate < 1)
+                setRate(rate + 0.02);
+        }, 4);
+        return () => {
+            clearInterval(id);
+        }
+    })
+
     return (
         <div class="w3-light-grey w3-round" style = {props.style}>
-            <div class="bar w3-round w3-red" style={{width : lay3 + "%"}}>
-                <div class="bar w3-round w3-green" style={{width : lay2 + "%"}}>
-                    <div class="bar w3-round w3-blue" style={{width : lay1 + "%"}}></div>
+            <div class="bar w3-round w3-red" style={{width : rate * lay3 + "%"}}>
+                <div class="bar w3-round w3-green" style={{width : rate * lay2 + "%"}}>
+                    <div class="bar w3-round w3-blue" style={{width : rate * lay1 + "%"}}></div>
                 </div>
             </div>
         </div>
