5bb9346413d8adde1724a226e667ab46a06e83eb
Marco Magnanini <marcomag416@gmail.com>
2021-12-26 20:02:37 +0100

    bozza dashboard

--- tesseramenti/src/components/dashboard/dashboard.js
+++ tesseramenti/src/components/dashboard/dashboard.js
@@ -4,40 +4,37 @@ import { API_BASE } from '../../config/config.js';*/
 import { fetch } from '../../functions/useFetch.js';
 /*import ReactDOM from 'react-dom';*/
 import { sessionContext } from '../context.js';
+import ProgressBar from '../elem/progressBar.js';
 
 function Dashboard(props) {
-    const [recived, setRecived] = useState("");
     const [token, setToken, deleteToken] = useContext(sessionContext);
-
-    function ValidaSessione() {
-        const send = { token: token };
-        const fetchData = async () => {
-            let data = await fetch("/sessionCheck", send);
-            //console.log(data);
-            if (data.status) {
-                setRecived(data.mail + data.squadra + data.lega + data.stagione);
-            }
-            else {
-                setRecived(data.msg);
-            }
-        }
-        fetchData();
-    }
-
-    function logOut() {
-        console.log("Logut...");
-        deleteToken();
-        /*localStorage.removeItem('token');
-        window.location.reload();*/
-    }
 
     return (
         <div className="w3-container">
-            <h1>Dashboard</h1>
-            <h3>Richiesta back-end</h3>
-            <p>{recived}</p>
-            <button onClick={() => ValidaSessione() }>Valida Sessione</button>
-            <button onClick={() => logOut()}>Log out</button>
+            <h2>Dashboard</h2>
+            <div className='w3-container w3-white'>
+                <section className='w3-padding-large'>
+                    <div className='w3-container w3-half'>
+                        <h4>Panoramica giocatori</h4>
+                        <ProgressBar style = {{width : "80%" }} blue = {0} green = {80} red = {3} />
+                        <div className='w3-padding'>
+                            <h6><b>Tesserati : 18 / 20</b></h6>
+                            <h6><b>Certificati validi : 16 / 20</b></h6>
+                        </div>
+                    </div>
+                    <div className = "w3-container w3-half">
+                        <h4>Panoramica dirigenti</h4>
+                        <ProgressBar style = {{width : "80%" }} blue = {75} green = {0} red = {0} />
+                        <h6>Tesserati : 3 / 4</h6>
+                    </div>
+                </section>
+
+                <div className='w3-container w3-padding-large w3-margin'>
+                    <h4>Invia elenco tesserati</h4>
+                    <p>Inviando l'elenco tesserati le informazioni relative a giocatori e dirigenti non potranno più essere modificate. Sarai comunque in grado di aggiungere nuovi certificati medici</p>
+                    <button className='w3-button w3-blue w3-round'>Invia elenco</button>
+                </div>
+            </div>
         </div>
         );
 
