fc60479e34344e98321957a0efc9a75f0b59958b
Marco Magnanini <marcomag416@gmail.com>
2021-12-27 17:51:10 +0100

    Implementato calcolo statistiche nella dashboard

--- tesseramenti/src/components/dashboard/dashboard.js
+++ tesseramenti/src/components/dashboard/dashboard.js
@@ -1,7 +1,7 @@
-import { React, useContext, useState } from 'react';
+import { React, useContext, useState, useMemo } from 'react';
 /*import axios from 'axios';
 import { API_BASE } from '../../config/config.js';*/
-import { fetch } from '../../functions/useFetch.js';
+import { useFetch } from '../../functions/useFetch.js';
 /*import ReactDOM from 'react-dom';*/
 import { sessionContext } from '../context.js';
 import ProgressBar from '../elem/progressBar.js';
@@ -9,6 +9,16 @@ import Alert from '../elem/alert.js';
 
 function Dashboard(props) {
     const [token, setToken, deleteToken] = useContext(sessionContext);
+    const fetchGio = useFetch("/elencoTesserati", {token : token}, null);
+    const fetchDir = useFetch("/elencoDirigenti", {token : token}, null);
+
+    const [certTot, certValidi, gioTot, gioValidi] = useMemo(() => calcolaStatGio(fetchGio), fetchGio);
+    const dirTot = useMemo(() => calcolaStatDir(fetchDir), fetchDir);
+
+    var invioOk = false;
+    if(gioValidi === 20 && dirTot <= 4 && dirTot >= 1){
+        invioOk = true;
+    }
 
     return (
         <div className="w3-container">
@@ -19,29 +29,30 @@ function Dashboard(props) {
                         <h4>Panoramica giocatori</h4>
                         <ProgressBar 
                             style = {{width : "80%" }} 
-                            blue = {60} green = {0} red = {25} 
+                            blue = {gioValidi / 20 * 100} green = {0} red = {(gioTot - gioValidi) / 20 * 100} 
                         />
                         <div className='w3-padding'>
-                            <h6>Tesserati : 18 / 20</h6>
-                            <h6>Giocatori completi : 16 / 20</h6>
+                            <h6>Tesserati : {gioTot} / 20</h6>
+                            <h6>Giocatori completi : {gioValidi} / 20</h6>
                         </div>
 
                         <h4>Certificati medici</h4>
                         <ProgressBar 
                             style = {{width : "80%" }} 
-                            blue = {40} green = {0} red = {20} 
+                            blue = {certValidi / 20 * 100} green = {0} red = {(certTot - certValidi) / 20 * 100} 
                         />
                         <div className='w3-padding'>
-                            <h6>Certificati inseriti / validi : 10 / 16</h6>
+                            <h6>Certificati inseriti : {certTot} / 20</h6>
+                            <h6>Certificati non validi : {certTot - certValidi}</h6>
                         </div>
                     </div>
                     <div className = "w3-container w3-half">
                         <h4>Panoramica dirigenti</h4>
                         <ProgressBar 
                             style = {{width : "80%" }} 
-                            blue = {75} green = {0} red = {0} 
+                            blue = {dirTot / 4 * 100} green = {0} red = {0} 
                         />
-                        <h6>Tesserati : 3 / 4</h6>
+                        <h6>Tesserati : {dirTot} / 4</h6>
                     </div>
                 </div>
                 
@@ -52,16 +63,56 @@ function Dashboard(props) {
                         mode = {"info"} 
                         msg = {"L'elenco tesserati deve essere inviato entro il 10/2/2022."}
                     />
-                    <Alert 
-                        mode={"alert"} 
-                        msg={"Tutti i giocatori devono essere completi per poter inviare l'elenco tesserati."}
-                    />
-                    <button className='w3-button w3-blue w3-round'>Invia elenco</button>
+                    {invioOk ? null : 
+                        <Alert 
+                            mode={"alert"} 
+                            msg={"Tutti i giocatori devono essere completi per poter inviare l'elenco tesserati."}
+                        />
+                    }
+                    <button className='w3-button w3-blue w3-round' disabled = {!invioOk} >Invia elenco</button>
                 </div>
             </div>
         </div>
         );
 
+}
+
+function calcolaStatGio(fetchGio) {
+    var certTot = 0, certValidi = 0, gioTot = 0, gioValidi = 0;
+    if (fetchGio[0].status && fetchGio[0].vett != undefined) {
+        const vett = fetchGio[0].vett;
+        console.log(vett);
+        let scadenza;
+        const dataOggi = Date.parse(new Date);
+        vett.forEach((x) => {
+            if(x.scadenza != null){
+                certTot ++;
+                scadenza = Date.parse(new Date(x.scadenza));
+                if(scadenza > dataOggi){
+                    certValidi++;
+                }
+            }
+            if(x.numero_maglia != null && x.ruolo != "" && x.taglia != ""){
+                gioValidi++;
+            }
+        });
+        gioTot = vett.length;
+    }
+    else {
+        console.log("Errore caricamento giocatori");
+    }
+    return [certTot, certValidi, gioTot, gioValidi];        
+}
+
+function calcolaStatDir(fetchDir){
+    if (fetchDir[0].status && fetchDir[0].vett != undefined) {
+        const vett = fetchDir[0].vett;
+        return vett.length;
+    }
+    else{
+        console.log("Errore caricamento dirigenti");
+        return 0;
+    }
 }
 
 
