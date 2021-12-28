b7e9423379e2166c01a2edcb4b977fd36c174706
Marco Magnanini <marcomag416@gmail.com>
2021-12-28 20:37:55 +0100

    Implementato reset password

--- api/src/amministratori/amministratori.php
+++ api/src/amministratori/amministratori.php
@@ -1,7 +1,10 @@
 <?php
     function updatePsw($session, $dbConnection){
         if(!isset($_POST['pswOld']) || !isset($_POST['psw'])){
-            return array("status" => false, "msg" => /*"Password non trovata"*/$_POST['pswOld']."  ".$_POST['psw']);
+            return array("status" => false, "msg" => "Password non trovata");
+        }
+        if(!controlloPsw($_POST['psw'])){
+            return array("status" => false, "msg" => "Password non valida");
         }
         $stm = $dbConnection -> prepare("SELECT psw FROM `amministratori` WHERE id = :idamm");
         $stm->bindParam(":idamm", $session['id']);
@@ -43,5 +46,70 @@
         }
     
         return array("status" => true, "msg" => "Elenco inviato");
+    }
+
+    function creaCodiceRipristino($idamm, $delay,  $dbConnection){
+        $raw_token = $idamm.strval(time()).random_bytes(32);
+        $token = password_hash($raw_token, PASSWORD_DEFAULT);
+
+        $d=strtotime("+$delay Days");
+
+        $sql = file_get_contents(ROOTPATH."\src\sqlQueries\insertCodiceRipristino.sql");
+        $stm = $dbConnection -> prepare($sql);
+        $stm->bindValue(":idamm", $idamm, PDO::PARAM_STR);
+        $stm->bindValue(":psw", $token, PDO::PARAM_STR);
+        $stm->bindValue(":scadenza", date("Y-m-d h:i:s", $d), PDO::PARAM_STR);
+        try{
+            $stm->execute();
+        }catch (\PDOException $e) {
+            $errmsg = $e->getMessage();
+            return "error";
+        }
+        return $token;
+    }
+
+    function resetPsw($dbConnection){
+        if(!isset($_POST['token']) || !isset($_POST['mail']) || !isset($_POST['psw']) || $_POST['token'] == "" || $_POST['mail'] == ""){
+            return array("status" => false, "msg" => "Parametri mancanti");
+        }
+
+        if(!controlloPsw($_POST['psw'])){
+            return array("status" => false, "msg" => "Password non valida");
+        }
+
+        $sql = "select distinct id_amministratore as idamm from codici_ripristino where token = :token;";
+        $stm = $dbConnection -> prepare($sql);
+        $stm->bindValue(":token", $_POST['token'], PDO::PARAM_STR);
+        try{
+            $stm->execute();
+        }catch (\PDOException $e) {
+            $errmsg = $e->getMessage();
+            return array("status" => false, "msg" => "Errore generico");
+        }
+
+        if($stm->rowCount() != 1){
+            return array("status" => false, "msg" => "Autorizzazione negata");
+        }
+        $row = $stm->fetch(PDO::FETCH_ASSOC);
+        
+        $psw =  password_hash($_POST['psw'], PASSWORD_DEFAULT);
+        $sql = file_get_contents(ROOTPATH."\src\sqlQueries\updatePsw.sql");
+        $stm = $dbConnection -> prepare($sql);
+        $stm->bindValue(":idamm", $row['idamm'], PDO::PARAM_STR);
+        $stm->bindValue(":psw", $_POST['psw'], PDO::PARAM_STR);
+        try{
+            $stm->execute();
+        }catch (\PDOException $e) {
+            $errmsg = $e->getMessage();
+            return array("status" => false, "msg" => "Errore sql : $errmsg");
+        }
+        return array("status" => true, "msg" => "Password aggiornata");
+    }
+
+    function controlloPsw($psw){
+        if(strlen($psw) < 8){
+            return false;
+        }
+        return true;
     }
 ?>
